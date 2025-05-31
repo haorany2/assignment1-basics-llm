af0ea41fa9e752784232268c7f6e845309b798b6
Your Name <you@example.com>
2025-05-31 07:52:02 +0000

    minor

--- cs336_basics/token_base.py
+++ cs336_basics/token_base.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 import copy
 import regex as re
 import os
+import pickle
 from pretokenization_example import find_chunk_boundaries
 GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
@@ -194,10 +195,11 @@ class Tokenizer:
 
     def __init__(self):
         # default: vocab size of 256 (all bytes), no merges, no patterns
-        self.merges = {} # (int, int) -> int
+        self.merges = [] # (int, int) -> int
         self.pattern = "" # str
         self.special_tokens = {} # str -> int, e.g. {'<|endoftext|>': 100257}
-        self.vocab = self._build_vocab() # int -> bytes
+        self.vocab = {} # int -> bytes
+        self.reverse_vocab = {}
 
     def train(self, text, vocab_size, verbose=False):
         # Tokenizer can train a vocabulary of size vocab_size from text
@@ -211,16 +213,16 @@ class Tokenizer:
         # Tokenizer can decode a list of integers into a string
         raise NotImplementedError
 
-    def _build_vocab(self):
-        # vocab is simply and deterministically derived from merges
-        vocab = {idx: bytes([idx]) for idx in range(256)}
-        for (p0, p1), idx in self.merges.items():
-            vocab[idx] = vocab[p0] + vocab[p1]
-        for special, idx in self.special_tokens.items():
-            vocab[idx] = special.encode("utf-8")
-        return vocab
+    # def _build_vocab(self):
+    #     # vocab is simply and deterministically derived from merges
+    #     vocab = {idx: bytes([idx]) for idx in range(256)}
+    #     for (p0, p1), idx in self.merges.items():
+    #         vocab[idx] = vocab[p0] + vocab[p1]
+    #     for special, idx in self.special_tokens.items():
+    #         vocab[idx] = special.encode("utf-8")
+    #     return vocab
 
-    def save(self, file_prefix):
+    def save(self, model_file):
         """
         Saves two files: file_prefix.vocab and file_prefix.model
         This is inspired (but not equivalent to!) sentencepiece's model saving:
@@ -228,40 +230,19 @@ class Tokenizer:
         - vocab file is just a pretty printed version for human inspection only
         """
         # write the model: to be used in load() later
-        model_file = file_prefix + ".model"
-        with open(model_file, 'w') as f:
-            # write the version, pattern and merges, that's all that's needed
-            f.write("minbpe v1\n")
-            f.write(f"{self.pattern}\n")
-            # write the special tokens, first the number of them, then each one
-            f.write(f"{len(self.special_tokens)}\n")
-            for special, idx in self.special_tokens.items():
-                f.write(f"{special} {idx}\n")
-            # the merges dict
-            for idx1, idx2 in self.merges:
-                f.write(f"{idx1} {idx2}\n")
-        # write the vocab: for the human to look at
-        vocab_file = file_prefix + ".vocab"
-        inverted_merges = {idx: pair for pair, idx in self.merges.items()}
-        with open(vocab_file, "w", encoding="utf-8") as f:
-            for idx, token in self.vocab.items():
-                # note: many tokens may be partial utf-8 sequences
-                # and cannot be decoded into valid strings. Here we're using
-                # errors='replace' to replace them with the replacement char �.
-                # this also means that we couldn't possibly use .vocab in load()
-                # because decoding in this way is a lossy operation!
-                s = render_token(token)
-                # find the children of this token, if any
-                if idx in inverted_merges:
-                    # if this token has children, render it nicely as a merge
-                    idx0, idx1 = inverted_merges[idx]
-                    s0 = render_token(self.vocab[idx0])
-                    s1 = render_token(self.vocab[idx1])
-                    f.write(f"[{s0}][{s1}] -> [{s}] {idx}\n")
-                else:
-                    # otherwise this is leaf token, just print it
-                    # (this should just be the first 256 tokens, the bytes)
-                    f.write(f"[{s}] {idx}\n")
+     
+        # Save multiple objects
+        with open(model_file, "wb") as f:
+            pickle.dump({
+                "merges": self.merges,
+                "vocab": self.vocab,
+                "reverse_vocab": self.reverse_vocab,
+                "special_tokens": self.special_tokens,
+                "version": "1.0",
+                "info": "Custom BPE model"
+            }, f)
+        print('---done---')
+       
 
     def load(self, model_file):
         """Inverse of save() but only for the model file"""
@@ -269,23 +250,17 @@ class Tokenizer:
         # read the model file
         merges = {}
         special_tokens = {}
+        vocab = {}
+        reverse_vocab = {}
         idx = 256
-        with open(model_file, 'r', encoding="utf-8") as f:
-            # read the version
-            version = f.readline().strip()
-            assert version == "minbpe v1"
-            # read the pattern
-            self.pattern = f.readline().strip()
-            # read the special tokens
-            num_special = int(f.readline().strip())
-            for _ in range(num_special):
-                special, special_idx = f.readline().strip().split()
-                special_tokens[special] = int(special_idx)
-            # read the merges
-            for line in f:
-                idx1, idx2 = map(int, line.split())
-                merges[(idx1, idx2)] = idx
-                idx += 1
+        with open(model_file, 'rb') as f:
+            model_data = pickle.load(f)
+        merges = model_data["merges"]
+        vocab = model_data["vocab"]
+        vocab = model_data["vocab"]
+        reverse_vocab = model_data["reverse_vocab"]
+        special_tokens = model_data["special_tokens"]
         self.merges = merges
         self.special_tokens = special_tokens
-        self.vocab = self._build_vocab()
+        self.vocab = vocab
+        self.reverse_vocab = reverse_vocab
